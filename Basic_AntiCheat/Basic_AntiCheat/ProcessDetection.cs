0f7ee6643fd00e228b2d23b4c45728747abdfec2
nuzy <94016889+nuzydev@users.noreply.github.com>
2024-07-25 13:02:08 -0300

    Add files via upload

---
+++ Basic_AntiCheat/Basic_AntiCheat/ProcessDetection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Basic_AntiCheat
+{
+
+
+    internal class ProcessDetection
+    {
+        public static bool detected;
+        public static List<string> currentRunning = new List<string>();
+        public static string[] suspectNames = ["cheat", "debugger"];
+        public static string ignore = AppDomain.CurrentDomain.FriendlyName;
+
+        public static void UpdateProcList()
+        {
+            Process[] running = Process.GetProcesses();
+            foreach (Process process in running)
+            {
+                currentRunning.Add(process.ProcessName);
+
+            }
+        }
+
+        public static void FindProcess()
+        {
+            foreach (string procName in currentRunning)
+            {
+                for (int i = 0; i < suspectNames.Length; i++)
+                {
+                    if (procName != ignore)
+                    {
+                        if (procName.Contains(suspectNames[i])){
+                            detected = true;
+                            Console.WriteLine("Detected" + suspectNames[i]);
+                            return;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Basic_AntiCheat/Basic_AntiCheat/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Basic_AntiCheat
+{
+    class Program
+    {
+        private static Thread thread;
+        static void Main(string[] args)
+        {
+            thread = new Thread(ConsoleThread);
+            thread.Start();
+            Console.ReadLine();
+        }
+
+        public static void ConsoleThread()
+        {
+            while (true)
+            {
+                ProcessDetection.UpdateProcList();
+                ProcessDetection.FindProcess();
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
